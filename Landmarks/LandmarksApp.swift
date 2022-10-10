0e86feac3959e2b5849994f4a1f0fba72807ee91
cagatay <cagatay.gedik@icloud.com>
2022-10-10 16:29:51 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Celil Çağatay Gedik on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c5766f317e9a2aba3f557b36908c235f86ecf591
cagatay <cagatay.gedik@icloud.com>
2022-10-18 13:35:11 +0300

    updated  ModelData

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -9,8 +9,18 @@ import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
- @Published var landmarks: [Landmark] = load("landmarkData.json")
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured}
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks, by: { $0.category.rawValue}
+        )
+    }
     
 }
 
